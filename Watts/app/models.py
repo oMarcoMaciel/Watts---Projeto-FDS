5c10ca5eaf48e480b2a75c205bd2122efc094597
GalileuCMMoares <galileumcalaca@gmail.com>
2024-09-28 19:47:08 -0300

    implementando Pontos de energia

--- Watts/app/models.py
+++ Watts/app/models.py
@@ -4,6 +4,11 @@ class Locacao(models.Model):
     estado = models.CharField(max_length=50) 
     nome = models.CharField(max_length=100)
 
+class PontoDeEnergia(models.Model):
+    estado = models.CharField(max_length=50) 
+    nome = models.CharField(max_length=100)
+    comodo= models.CharField(max_length=100)
+    gastos = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
 
 #class Comodo(models.Model):
--- Watts/app/urls.py
+++ Watts/app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'app'
 
 urlpatterns = [
     path('', views.HomeViews.as_view(), name ='home'),
     path('addLocacao/', views.AddLocacao.as_view(), name = 'addLocacao'),
+    path('PontoDeEnergia/', views.PontoDeEnergiaView.as_view(), name = 'PontoDeEnergia'),
 ]
