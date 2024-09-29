02a5ebc0a688c381642f2402a9f7b00a82501bf3
Antônio Paes <paesdeandradeantonio@gmail.com>
2024-09-29 19:02:11 -0300

    Implementando o backend da 2 historia

--- Watts/app/urls.py
+++ Watts/app/urls.py
@@ -6,4 +6,5 @@ app_name = 'app'
 urlpatterns = [
     path('', views.HomeViews.as_view(), name ='home'),
     path('addLocacao/', views.AddLocacao.as_view(), name = 'addLocacao'),
+    path('addComodo/', views.CriarComodo, name = 'addComodo'),
 ]
