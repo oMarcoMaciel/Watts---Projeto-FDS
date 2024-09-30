8e1038dc64362b80b92cd2753a1fe5240639ab9d
Antônio Paes <paesdeandradeantonio@gmail.com>
2024-09-30 14:16:56 -0300

    Atualizando workflow

--- Watts/Watts/settings.py
+++ Watts/Watts/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "whitenoise.runserver_nostatic",
+    'whitenoise.runserver_nostatic',
     'app',
     'usuarios',
 ]
