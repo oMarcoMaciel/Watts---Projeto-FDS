0c78ebf2fa42ada1c8f556a34634b89bee316609
Antônio Paes <paesdeandradeantonio@gmail.com>
2024-10-22 20:48:51 -0300

    Resolvendo erros Build

--- Watts/Watts/settings.py
+++ Watts/Watts/settings.py
@@ -79,7 +79,7 @@ ROOT_URLCONF = 'Watts.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'app', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
