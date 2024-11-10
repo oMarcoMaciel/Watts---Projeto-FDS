bfa729ad87b55b63c811e33b76ba3b6a6f2189fe
João Henrique <167437961+jhrvo0@users.noreply.github.com>
2024-11-10 16:07:20 -0300

    test

--- Watts/cypress/e2e/4RemovePontoDeEnergia.cy.js
+++ Watts/cypress/e2e/4RemovePontoDeEnergia.cy.js
@@ -5,11 +5,12 @@ describe('Remover Ponto de Energia', () => {
   
     it('Remover Ponto de Energia', () => {
         cy.get('form[action*="deletePontodeenergia"]').each(($form, index, $list) => {
-            cy.wrap($form).find('button').click();
-            if (index < $list.length - 1) {
-                cy.wait(1000); // Aguarde um segundo para a página ser recarregada
-                cy.reload(); // Recarrega a página para garantir que o elemento foi removido
-            }
+            cy.wrap($form).find('button').click().then(() => {
+                if (index < $list.length - 1) {
+                    cy.wait(1000); // Aguarde um segundo para a página ser recarregada
+                    cy.reload(); // Recarrega a página para garantir que o elemento foi removido
+                }
+            });
         });
     });
 });
--- Watts/cypress/e2e/5removeComodo.cy.js
+++ Watts/cypress/e2e/5removeComodo.cy.js
@@ -5,11 +5,12 @@ describe('Remover Cômodo', () => {
   
     it('Remover Cômodo', () => {
         cy.get('form[action*="deleteComodo"]').each(($form, index, $list) => {
-            cy.wrap($form).find('button').click();
-            if (index < $list.length - 1) {
-                cy.wait(1000); // Aguarde um segundo para a página ser recarregada
-                cy.reload(); // Recarrega a página para garantir que o elemento foi removido
-            }
+            cy.wrap($form).find('button').click().then(() => {
+                if (index < $list.length - 1) {
+                    cy.wait(1000); // Aguarde um segundo para a página ser recarregada
+                    cy.reload(); // Recarrega a página para garantir que o elemento foi removido
+                }
+            });
         });
     });
 });
